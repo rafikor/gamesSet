28ef9feddc6488d3c10746d91d08254a6e04ad06
Arlou <orlovraf@gmail.com>
2023-03-31 21:43:59 +0300

    tick tack toe: display field of buttons

--- gamesSet/ClientApp/src/index.js
+++ gamesSet/ClientApp/src/index.js
@@ -3,7 +3,7 @@ import { render } from 'https://cdn.skypack.dev/react-dom'
 
 import "./custom.css"
 
-const styleSquare = {
+const styleButton = {
 	background: "lightblue",
 	border: "2px solid darkblue",
 	fontSize: "30px",
@@ -12,14 +12,26 @@ const styleSquare = {
 	outline: "none",
 };
 
+const style = {
+	border: "4px solid darkblue",
+	borderRadius: "10px",
+	width: "250px",
+	height: "250px",
+	margin: "0 auto",
+	display: "grid",
+	gridTemplate: "repeat(3, 1fr) / repeat(3, 1fr)",
+};
+
+
 const Square = ({ value, onClick }) => (
-	<button style={styleSquare} onClick={onClick}>
+	<button style={styleButton} onClick={onClick}>
 		{value}
 	</button>
 );
 
+
 const Game = ({ onClick}) => (
-	<div>
+	<div style={style}>
 		<Square value="1" onClick={() => onClick("dummy value")} />
 		<Square value="2" onClick={() => onClick("dummy value")} />
 		<Square value="3" onClick={() => onClick("dummy value")} />
