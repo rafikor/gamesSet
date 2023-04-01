69f1afd70bea3940ecaff87c81ee75e604b9f311
Arlou <orlovraf@gmail.com>
2023-04-01 13:35:53 +0300

    Add model for the database

---
+++ gamesSet/Models/GameSession.cs
@@ -0,0 +1,15 @@
+﻿namespace gamesSet.Models
+{
+    public class GameSession
+    {
+        public int Id { get; set; }
+        public string UserCreator { get; set; }
+        public string SecondUser { get; set; }
+        public string GameParams;
+        public string WinnerName;
+        public SessionStatus status;
+        public DateTime creationTime;
+    }
+
+    public enum SessionStatus {created, activeGame,finished,cancelled};
+}
