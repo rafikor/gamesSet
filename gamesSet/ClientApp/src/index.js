e954022895c6189ce352c933640ff8e781ecd0c9
Arlou <orlovraf@gmail.com>
2023-04-01 13:35:37 +0300

    Add simple start page with some buttons

--- gamesSet/ClientApp/src/index.js
+++ gamesSet/ClientApp/src/index.js
@@ -1,13 +1,41 @@
-import { StrictMode } from 'react';
+import { StrictMode, Fragment } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import { BrowserRouter } from 'react-router-dom';
 import './custom.css';
 
-import App from './App';
 
 const rootElement = document.getElementById('app');
 const root = createRoot(rootElement);
 
+const PlayerName = () => {
+    return <input type="text" id="playername" placeholder="Input your name..."></input>
+}
+
+const GamesListToCreateSession = ({ games }) => {
+    return (<ul>
+        {games.map(({gameName},id) => (
+            <li key={id }>
+                <button id={id}>Create session for {gameName}</button>
+        </li>
+        ))}
+    </ul>)
+}
+
+const SessionsToPlayList = ({ sessions }) => {
+    return (<ul>
+        {sessions.map(({ sessionId, gameName }, id) => (
+            <li key={id}>
+                <button id={id}>Connect to session #{sessionId} for {gameName}</button>
+            </li>
+        ))}
+    </ul>)
+}
+
 root.render(
-        <App />);
+    <Fragment>
+        <PlayerName />
+        <GamesListToCreateSession games={[{ gameName: "Tick-Tack-Toe" }, { gameName: "Labyrinth" }]} />
+        <SessionsToPlayList sessions={[{ gameName: "Tick-Tack-Toe" , sessionId:2}, { gameName: "Labyrinth", sessionId:4 }]} />
+    </Fragment>
+);
