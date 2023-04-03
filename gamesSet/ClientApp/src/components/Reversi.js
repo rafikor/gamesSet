9effc223838673bc9ff4a5a295b85cce178a5c7c
Arlou <orlovraf@gmail.com>
2023-04-03 10:25:07 +0300

    Update for the Reversi game

--- gamesSet/ClientApp/src/components/Reversi.js
+++ gamesSet/ClientApp/src/components/Reversi.js
@@ -5,6 +5,7 @@ import './Reversi.css';
 export function Reversi() {
     const [board, setBoard] = useState(Array(8).fill(Array(8).fill(null)));
     const [player, setPlayer] = useState('black');
+    const [winner, setWinner] = useState('');
 
     useEffect(() => {
         const newBoard = board.map((rowArray, row) =>
@@ -27,74 +28,102 @@ export function Reversi() {
         setBoard(newBoard);
     }, []);
 
-    function handleClick(row, col) {
+    function handleClick(row, col,board, setBoard, setWinner) {
         if (board[row][col] !== null) {
             return;
         }
 
-        let newBoard = board.map((rowArray) => [...rowArray]);
-
+        let flippedCells = [];
+        // Check in all 8 directions from the clicked cell for any opponent pieces
         const directions = [
-            [0, 1],
-            [0, -1],
-            [1, 0],
-            [-1, 0],
-            [1, 1],
-            [1, -1],
-            [-1, 1],
-            [-1, -1],
+            [-1, -1], [-1, 0], [-1, 1],
+            [0, -1], [0, 1],
+            [1, -1], [1, 0], [1, 1]
         ];
-
-        let canFlip = false;
-
-        directions.forEach((direction) => {
-            const [dx, dy] = direction;
-            let x = row + dx;
-            let y = col + dy;
-            let flippedCells = [];
-
-            while (x >= 0 && x < 8 && y >= 0 && y < 8) {
-                if (board[x][y] === null) {
-                    break;
-                }
-
-                if (board[x][y] !== player) {
-                    flippedCells.push([x, y]);
-                } else {
-                    if (flippedCells.length > 0) {
-                        canFlip = true;
-                        flippedCells.forEach(([x, y]) => {
-                            newBoard[x][y] = player;
-                        });
-                    }
-                    break;
-                }
-
-                x += dx;
-                y += dy;
+        for (const dir of directions) {
+            let r = row + dir[0];
+            let c = col + dir[1];
+            let tempFlippedCells = [];
+            // Check if the adjacent cell is an opponent piece
+            while (
+                r >= 0 && r < 8 &&
+                c >= 0 && c < 8 &&
+                board[r][c] !== null &&
+                board[r][c] !== player
+            ) {
+                tempFlippedCells.push([r, c]);
+                r += dir[0];
+                c += dir[1];
             }
-        });
-
-        if (canFlip) {
-            newBoard[row][col] = player;
-            setBoard(newBoard);
-            setPlayer(player === 'black' ? 'white' : 'black');
+            //If are opponent pieces in this direction that can be flipped
+            if (
+                r >= 0 && r < 8 &&
+                c >= 0 && c < 8 &&
+                board[r][c] === player
+            ) {
+                flippedCells.push(...tempFlippedCells);
+            }
         }
+
+        //move is invalid
+        if (flippedCells.length === 0) {
+            return;
+        }
+
+        //Update the board
+        const newBoard = board.map((rowArray, r) =>
+            rowArray.map((cell, c) =>
+                (r === row && c === col) || flippedCells.some(cell => cell[0] === r && cell[1] === c)
+                    ? player
+                    : cell
+            )
+        );
+        setBoard(newBoard);
+        setPlayer(player === 'black' ? 'white' : 'black');
+
+        //search winner
+        const flattenedBoard = newBoard.flat();
+        const numEmptyCells = flattenedBoard.filter(cell => cell === null).length;
+        const numWhiteCells = flattenedBoard.filter(cell => cell === 'white').length;
+        const numBlackCells = flattenedBoard.filter(cell => cell === 'black').length;
+        let winner = null;
+        if (numEmptyCells === 0 || numWhiteCells === 0 || numBlackCells === 0) {
+            //If there are no empty cells or one color has no pieces, the game is over
+            if (numWhiteCells === numBlackCells) {
+                winner = 'tie';
+            } else {
+                winner = numWhiteCells > numBlackCells ? 'white' : 'black';
+            }
+        }
+        setWinner(winner);
     }
 
     return (
-        <div className="game-board">
-            {board.map((row, rowIndex) => (
-                <div key={rowIndex} className="row">
-                    {row.map((cell, colIndex) => (
-                        <div
-                            key={colIndex}
-                            className={`cell ${cell}-piece`}
-                            onClick={() => handleClick(rowIndex, colIndex)}
-                        />
-                    ))}
-                </div>
-            ))}
+        <div>
+            <div className="game-board">
+                {board.map((row, rowIndex) => (
+                    <div key={rowIndex} className="row">
+                        {row.map((cell, colIndex) => (
+                            <div
+                                key={colIndex}
+                                className={`cell ${cell}-piece`}
+                                onClick={() => handleClick(rowIndex, colIndex, board, setBoard, setWinner)}
+                            />
+                        ))}
+                    </div>
+                ))}
+            </div>
+            <div className="status">
+                {winner ? (
+                    winner === 'tie' ? (
+                        <p>It's a tie!</p>
+                    ) : (
+                        <p>{winner === 'black' ? 'Black' : 'White'} wins!</p>
+                    )
+                ) : (
+                        <p>{player === 'black' ? 'Black' : 'White'}'s turn</p>
+                )}
+            </div>
         </div>
     );
 }
