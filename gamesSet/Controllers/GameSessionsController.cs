abf68f1454440ebd183b9b5d2711b125803c4a88
Arlou <orlovraf@gmail.com>
2023-04-04 00:28:40 +0300

    Remove unnecessary code from GameSessionsController.cs

--- gamesSet/Controllers/GameSessionsController.cs
+++ gamesSet/Controllers/GameSessionsController.cs
@@ -29,66 +29,6 @@ namespace gamesSet.Controllers
             _sp = sp;
         }
 
-        // GET: api/GameSessions
-//        [HttpGet]
-        public async Task<ActionResult<IEnumerable<GameSession>>> GetGameSession()
-        {
-          if (_context.GameSession == null)
-          {
-              return NotFound();
-          }
-            return await _context.GameSession.ToListAsync();
-        }
-
-        // GET: api/GameSessions/5
-        //[HttpGet("{id}")]
-        public async Task<ActionResult<GameSession>> GetGameSession(int id)
-        {
-          if (_context.GameSession == null)
-          {
-              return NotFound();
-          }
-            var gameSession = await _context.GameSession.FindAsync(id);
-
-            if (gameSession == null)
-            {
-                return NotFound();
-            }
-
-            return gameSession;
-        }
-
-        // PUT: api/GameSessions/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        //[HttpPut("{id}")]
-        public async Task<IActionResult> PutGameSession(int id, GameSession gameSession)
-        {
-            if (id != gameSession.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(gameSession).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!GameSessionExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
         private async Task AddGameSession(GameSession gameSession)
         {
           if (_context.GameSession == null)
@@ -99,8 +39,6 @@ namespace gamesSet.Controllers
             _context.SaveChanges();
         }
 
-        // POST: api/GameSessions
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpGet]
         public IActionResult CreateSessionByUser(string userName="", int gameId=0)
         {
@@ -180,30 +118,6 @@ namespace gamesSet.Controllers
             }
             return dataToSend;
         }
-
-        // DELETE: api/GameSessions/5
-        //[HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteGameSession(int id)
-        {
-            if (_context.GameSession == null)
-            {
-                return NotFound();
-            }
-            var gameSession = await _context.GameSession.FindAsync(id);
-            if (gameSession == null)
-            {
-                return NotFound();
-            }
-
-            _context.GameSession.Remove(gameSession);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool GameSessionExists(int id)
-        {
-            return (_context.GameSession?.Any(e => e.Id == id)).GetValueOrDefault();
-        }
+        
     }
 }
